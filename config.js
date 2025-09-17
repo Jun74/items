0845d437db7fe48c86e034e8a16b0c0c6c32cf0c
Jun74 <yenasystem@gmail.com>
2025-09-17 22:32:43 +1200

    Add files via upload

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // 1) 시트를 CSV로 공개한 주소를 아래 변수에 붙여넣으세요.
 //    예) https://docs.google.com/spreadsheets/d/XXXX/pub?gid=0&single=true&output=csv
 //    또는  https://docs.google.com/spreadsheets/d/XXXX/export?format=csv&gid=0
-const CSV_URL = "https://drive.google.com/file/d/1Rk3UVjvUTf3Y0NIac-ajwXsvQMy2S_WF/view?usp=sharing";
+const CSV_URL = "https://drive.google.com/file/d/1Rk3UVjvUTf3Y0NIac-ajwXsvQMy2S_WF/view?usp=drive_link";
